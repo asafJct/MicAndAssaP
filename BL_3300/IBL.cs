8eeca195ebb08f1006c78d035dec913c03f44443
asafJct <44846049+asafJct@users.noreply.github.com>
2019-01-31 20:29:40 +0200

    Add files via upload

---
+++ BL_3300/IBL.cs
@@ -0,0 +1,51 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+   public interface IBL
+    {
+        int calculate_age(DateTime t);//gets an birthday
+                                      //return the age of the person.
+        #region Tester
+
+        bool findTester(int tt);//gets a key id address
+                                //return if he exists.
+        void addTester(Tester t);//gets a new object from type tester
+                                 //then add this tester to the data source.
+        void deleteTester(Tester t);//gets an exist object from type tester
+                                    //then delete it.
+        void updateTester(Tester t);//update an exist data.
+                                    
+        IEnumerable<Tester> getAllTester(Func<Tester, bool> predicat = null);//gets predicate , this function uses the Data source
+                                                                             //recive all appropriate testers  
+        List<Tester> InRange(string _address);//gets an address
+                                              //then return all Tester in the range
+                                              //it is done by comparition to the Tester key maximum range.
+
+        #endregion
+
+        #region Trainee
+        bool findTrainee(Trainee dd);
+        void addTrainee(Trainee od);  
+        void deleteTrainee(BE.Tester o);   //
+        void updateTrainee(BE.Tester o);  //עדכון הזמנה
+        IEnumerable<Trainee> getAllTrainee(Func<Tester, bool> predicat = null);//נבחנים
+        int numberOfTest(Trainee t);
+        bool  eligible(Trainee t);
+        #endregion
+        #region Test
+
+        bool findTest(Test dd);
+        void addTest(Test y);
+        void deleteTest(Test od);  //מחיקת מנה_מוזמנת
+        IEnumerable<Test> getAllTest(Func<Tester, bool> predicat = null);//מבחנים
+        #endregion
+        
+    }
+}
+
---
+++ PLWPF/AddTester.xaml.cs
@@ -0,0 +1,56 @@
+﻿using BL_3300;
+using DAL;
+using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for AddTester.xaml
+    /// </summary>
+    public partial class AddTester : Window
+    {
+        BE.Tester tester;
+        BL.BL_imp bl;
+        public AddTester()
+        {
+            InitializeComponent();
+            tester = new BE.Tester();
+            this.gridAddTester.DataContext = tester;
+            bl = BL.FactoryBL.getBL();
+            this.comboBox1.ItemsSource = Enum.GetValues(typeof(BE.gender));
+        }
+
+        private void addBranchButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.addBranch(branch);
+                MessageBox.Show("the branch \"" + branch.branchName + "\" added to the list", "");
+                branch = new BE.Branch();
+                this.gridAddBranch.DataContext = branch;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
